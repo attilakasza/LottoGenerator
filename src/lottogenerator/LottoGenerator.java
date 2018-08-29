5556ec5ca5074dfdbe87f64d8bb578ba46feb60e
Attila <9ttila.kasza@gmail.com>
2018-08-29 07:31:58 +0200

    feat: Create borderless window

--- src/lottogenerator/LottoGenerator.java
+++ src/lottogenerator/LottoGenerator.java
@@ -1,21 +1,45 @@
 package lottogenerator;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class LottoGenerator extends Application {
     
+    private double xOffset = 0;
+    private double yOffset = 0;
+ 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        
+        //make it movable
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
